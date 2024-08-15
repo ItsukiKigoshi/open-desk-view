5d0eb037f55a7437949a3b4156ed85f9bc3cbb55
sakuraimisato <blueberry112358@gmail.com>
2024-08-15 14:36:40 +0900

    testVideo1 change aspect

--- src/testVideo1.py
+++ src/testVideo1.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import math
+import argparse
 
 # カメラの高さと目標の座標を設定
 camera_height = 100
@@ -47,19 +48,10 @@ D_trans = [width/2,Y]
 print(A_trans)
 
 def order_points(pts):
-	# initialzie a list of coordinates that will be ordered
-	# such that the first entry in the list is the top-left,
-	# the second entry is the top-right, the third is the
-	# bottom-right, and the fourth is the bottom-left
 	rect = np.zeros((4, 2), dtype = "float32")
-	# the top-left point will have the smallest sum, whereas
-	# the bottom-right point will have the largest sum
 	s = pts.sum(axis = 1)
 	rect[0] = pts[np.argmin(s)]
 	rect[2] = pts[np.argmax(s)]
-	# now, compute the difference between the points, the
-	# top-right point will have the smallest difference,
-	# whereas the bottom-left will have the largest difference
 	diff = np.diff(pts, axis = 1)
 	rect[1] = pts[np.argmin(diff)]
 	rect[3] = pts[np.argmax(diff)]
@@ -71,14 +63,20 @@ def order_points(pts):
 p_original = np.float32([A, B, C, D])
 p_trans = np.float32([A_trans, B_trans, C_trans, D_trans])
 
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", help = "path to the image file")
+ap.add_argument("-c", "--coords",
+	help = "comma seperated list of source points")
+args = vars(ap.parse_args())
+pts = np.array(eval(args["coords"]), dtype = "float32")
+
 rect = order_points(pts)
 (tl, tr, br, bl) = rect
 widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
 widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
 maxWidth = max(int(widthA), int(widthB))
-# compute the height of the new image, which will be the
-# maximum distance between the top-right and bottom-right
-# y-coordinates or the top-left and bottom-left y-coordinates
+
 heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
 heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
 maxHeight = max(int(heightA), int(heightB))
@@ -105,6 +103,7 @@ while True:
 
     # 表示
     cv2.imshow('変換後の映像', frame_trans)
+    # cv2.imshow('変換前の映像', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
