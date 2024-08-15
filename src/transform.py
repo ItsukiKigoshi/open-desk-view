ca952d4887d97f68f9074b1bc781ea4ad4b37a6b
sakuraimisato <blueberry112358@gmail.com>
2024-08-15 17:56:30 +0900

    new order points

--- src/transform.py
+++ src/transform.py
@@ -2,25 +2,52 @@
 
 import numpy as np
 import cv2
+# def order_points(pts):
+# 	# initialzie a list of coordinates that will be ordered
+# 	# such that the first entry in the list is the top-left,
+# 	# the second entry is the top-right, the third is the
+# 	# bottom-right, and the fourth is the bottom-left
+# 	rect = np.zeros((4, 2), dtype = "float32")
+# 	# the top-left point will have the smallest sum, whereas
+# 	# the bottom-right point will have the largest sum
+# 	s = pts.sum(axis = 1)
+# 	rect[0] = pts[np.argmin(s)]
+# 	rect[2] = pts[np.argmax(s)]
+# 	# now, compute the difference between the points, the
+# 	# top-right point will have the smallest difference,
+# 	# whereas the bottom-left will have the largest difference
+# 	diff = np.diff(pts, axis = 1)
+# 	rect[1] = pts[np.argmin(diff)]
+# 	rect[3] = pts[np.argmax(diff)]
+# 	# return the ordered coordinates
+# 	print("rect:",rect)
+# 	return rect
+
+
 def order_points(pts):
-	# initialzie a list of coordinates that will be ordered
-	# such that the first entry in the list is the top-left,
-	# the second entry is the top-right, the third is the
-	# bottom-right, and the fourth is the bottom-left
-	rect = np.zeros((4, 2), dtype = "float32")
-	# the top-left point will have the smallest sum, whereas
-	# the bottom-right point will have the largest sum
-	s = pts.sum(axis = 1)
-	rect[0] = pts[np.argmin(s)]
-	rect[2] = pts[np.argmax(s)]
-	# now, compute the difference between the points, the
-	# top-right point will have the smallest difference,
-	# whereas the bottom-left will have the largest difference
-	diff = np.diff(pts, axis = 1)
-	rect[1] = pts[np.argmin(diff)]
-	rect[3] = pts[np.argmax(diff)]
-	# return the ordered coordinates
-	return rect
+    rect = np.zeros((4, 2), dtype = "float32")
+    # x の値でソート
+    sorted_indices = np.argsort(pts[:, 0])
+    sorted_data = pts[sorted_indices]
+
+    # リストを前後2つに分割
+    first_half = sorted_data[:2]
+    second_half = sorted_data[2:]
+
+    # y の値でソート
+    first_half_sorted = first_half[np.argsort(first_half[:, 1])]
+    second_half_sorted = second_half[np.argsort(second_half[:, 1])]
+
+    rect[0] = first_half_sorted[0]
+    rect[3] = first_half_sorted[1]
+
+    rect[1] = second_half_sorted[0]
+    rect[2] = second_half_sorted[1]
+
+    return rect
+
+
+
 
 def four_point_transform(image, pts):
 	# obtain a consistent order of the points and unpack them
